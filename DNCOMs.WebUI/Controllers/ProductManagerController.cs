354f403b9554f091f4f8ec2118a5e2a69bd768ec
Yov00 <yovelincho@gmail.com>
2021-01-26 22:53:40 +0200

    ProductsManager

---
+++ DNCOMs.WebUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DNCOMs.Core.Models;
+using DNCOMs.DataAcces.InMemory;
+
+namespace DNCOMs.WebUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+     
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
